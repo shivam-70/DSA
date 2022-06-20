80f62db821834c4e86d42152730390987752b5b4
shivam-70 <61525375+shivam-70@users.noreply.github.com>
2022-06-20 19:28:21 +0530

    Time: 826 ms (14.83%), Space: 157.8 MB (23.05%) - LeetHub

--- 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.cpp
+++ 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.cpp
@@ -1,31 +1,28 @@
 class Solution {
 public:
+    
+    /*
+    Hame har level ka maximum chahiye
+    */
+    unordered_map<int,vector<int>> adj;
+    int ans=0,duration=0;
+    void dfs(int manager,vector<int>& informTime)
+    {
+        ans=max(ans,duration);
+        for(auto it : adj[manager])
+        {
+            duration+=informTime[manager];
+            dfs(it,informTime);
+            duration-=informTime[manager]; // waapis usee level ki value laane ke liye    
+        }
+    }
+    
     int numOfMinutes(int n, int headID, vector<int>& manager, vector<int>& informTime) {
-        unordered_map<int,vector<int>> adj; //isliye liye hai kahe ki hamko negative value ke saath store krna hai aur iski 
-         //aadat daal lo kahe ki non continuous nodes ke liye efficient rhta hai chutiye.
         for(int i=0;i<n;i++)
         {
             adj[manager[i]].push_back(i);
         }
-        queue<pair<int,int>> q;// node and duration( how much time we have taken to reach there)
-        q.push({headID,0}); // head me first time zero sec me phoche. ab dkehenge kitna time info dispatch krne me lageyaga
-        int ans=0;
-        while(!q.empty())
-        {
-            int sie=q.size();
-            while(sie--){
-            int node=q.front().first;
-            int timeTaken=q.front().second;
-            q.pop();
-            int duration=timeTaken+informTime[node];//ye duration info dispatch krne me lageyga aage.
-            ans=max(ans,duration);
-            for(auto it : adj[node])
-            {
-                q.push({it,duration}); // aage ke saare nodes me abhi hi to pahche ho. Ab aage kb dispatc karega ye aage 
-                // jab queue se dispatch krna to dekhna.
-            }
-            }
-        }
+        dfs(headID,informTime);
         return ans;
     }
 };
