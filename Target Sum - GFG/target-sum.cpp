806aa38a8be4664c3590762b879620da8117ff5f
shivam-70 <61525375+shivam-70@users.noreply.github.com>
2022-06-24 19:27:43 +0530

    Added solution - LeetHub

---
+++ Target Sum - GFG/target-sum.cpp
@@ -0,0 +1,72 @@
+// { Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+//Back-end complete function Template for C++
+
+class Solution {
+public:
+    
+    unordered_map<string,int> dp; //Here as per the constraints if I took a 2D integer matrix then
+    //the problem was that the target might be negative certain times and negative index will
+    // give me error then in use unordered map for it whose solution is goven in the duscussion
+    // Section.
+    
+    
+    int helper(vector<int>& nums,int target,int i)
+    {
+        if(i==nums.size())
+        {
+            if(target==0)
+                return 1;
+            
+            return 0;
+        }
+        
+        string key=to_string(target)+" "+to_string(i);
+        
+        if(dp.find(key)!=dp.end())
+        {
+            return dp[key];
+        }
+        
+        int ans=helper(nums,target-nums[i],i+1)+helper(nums,target+nums[i],i+1);
+        
+        return dp[key]=ans;
+    }
+    
+    int findTargetSumWays(vector<int>& nums, int target) {
+        
+        
+        return helper(nums,target,0);
+    
+    }
+};
+
+// { Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N;
+        
+        cin>>N;
+        vector<int>arr(N);
+        
+        for(int i=0 ; i<N ; i++){
+            cin>>arr[i];
+        }
+        int target;
+        cin >> target;
+
+        Solution ob;
+        cout<<ob.findTargetSumWays(arr,target);
+        cout<<"\n";
+    }
+    return 0;
+}  // } Driver Code Ends
